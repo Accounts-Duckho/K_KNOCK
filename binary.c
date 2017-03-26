ef41bdbec545f2d48f5d3bc35e43c92b89eeb539
chocotime <duckhoim@daum.net>
2017-03-27 06:40:46 +0900

    use >0 instead of !=0 for stability

--- binary.c
+++ binary.c
@@ -2,34 +2,38 @@
 int main(void) {
     int input;
     int choice;
-    int num, i, j, n=0;
-    printf(" 수 변환\n");
+    int num, i, j;
+    int n=0;
+
+    printf(" 수변환\n");
     printf(" 1. 2진수\n");
     printf(" 2. 8진수\n");
     printf(" 3. 16진수\n");
     printf(" 선택 : ");
     scanf("%d", &choice);
 
-    printf(" 값 입력 : ");
+    printf(" 수 입력: ");
     scanf("%d", &input);
+
     switch(choice) {
         case 1:
+            printf("변환결과 : ");
+            if(input<2) {
+                printf("%d\n", input);
+                break;
+            }
             // find closest 2^n
             for(i=input; i>=2; i/=2)
                 n++;
-
-            printf(" 변환결과 : ");
-
-            // translating...
+            int value=input;
             for(i=n; i>=0; i--) {
-                // get 2^n
+                // cal 2^n for compare
                 num=1;
-                for(j=i; j!=0; j--)
+                for(j=i; j>0; j--)
                     num*=2;
-
-                if(input>=num) {
+                if(value>=num) {
                     printf("1");
-                    input-=num;
+                    value -= num;
                 }
                 else
                     printf("0");
@@ -48,6 +52,6 @@ int main(void) {
         default:
             printf(" 입력값 오류\n");
             break;
-        }
+    }
     return 0;
 }
