f6b28f9355c2aa1d345fe232d590a9e35f5d4a7a
chocotime <duckhoim@daum.net>
2017-03-27 06:43:36 +0900

    use if , %*s reduce for statement ( improve )
    + >0 use instead of !=0

--- make_dia.c
+++ make_dia.c
@@ -1,69 +1,67 @@
 #include <stdio.h>
 int main(void) {
-    int choice, size, isOdd;
+    int choice;
+    int size;
+    int isOdd;
     int i, j, line, comp;
-    printf(" 출력 방식 선택 \n");
-    printf(" 1. 특수문자로 직접 출력\n");
+
+    printf(" 출력 방식 선택\n");
+    printf(" 1. *로 직접 출력\n");
     printf(" 2. 공백으로 간접 출력\n");
     printf(" : ");
     scanf("%d", &choice);
 
     printf(" 크기 입력 : ");
     scanf("%d", &size);
-    isOdd=(size&1) ? 1 : 0;
+    //&연산은 둘다 1이면 1 
+    isOdd=(size&1);
 
     switch(choice) {
         case 1:
-
-            i=(isOdd) ? 1 : 2;
+            i = (isOdd) ? 1 : 2; // 초기 값
+            int len = (isOdd) ? size : size-1;
+            // *의 개수가 감소하기 시작하는 부분
             comp = (isOdd) ? (size+1)/2 : size/2;
-            for(line=1; line<=size; line++) {
-                if(i==0) break; // 짝수일때 생기는 마지막 한줄제거
-                for(j=(size-i)/2; j!=0; j--)
-                    printf(" ");
-                for(j=i; j!=0; j--)
-                    printf("%c", '*');
+
+            for(line=1; line<=len; line++) {
+                printf("%*s", (size-i)/2, "");
+                for(j=i; j>0; j--)
+                    printf("*");
                 printf("\n");
-                if(line<comp)
-                    i+=2;
-                else
-                    i-=2;
+
+                if(line<comp) i+=2;
+                else i-=2;
             }
             break;
 
         case 2:
-            //  문자 + 공백 + 문자
-            i=(isOdd) ? 1 : 2;
-            int middle=size-2;
+            // 문자 + 공백 + 문자 
+            i = (isOdd) ? 1 : 2;
+            // 양끝은 제외하고 중간만 본다.
+            int middle = size-2;
+
             comp = (isOdd) ? (middle+1)/2 : middle/2;
-
-            for(j=size; j!=0; j--) // 양 끝은 어짜피 size와 동일한 길이
-                printf("%c", '*');
+            for(j=size; j>0; j--)
+                printf("*");
             printf("\n");
-
-            for(line=1; line<=middle; line++) {
-                for(j=(size-i)/2; j!=0; j--)
-                    printf("%c", '*');
-                for(j=i; j!=0; j--)
-                    printf(" ");
-                for(j=(size-i)/2; j!=0; j--)
-                    printf("%c", '*');
+            for(line=1;line<=middle;line++) {
+                for(j=1; j<=size; j++) {
+                    if(j<=(size-i)/2) printf("*");
+                    else if(j<=(size+i)/2) printf(" ");
+                    else printf("*");
+                }
                 printf("\n");
-                if(line<comp)
-                    i+=2;
-                else
-                    i-=2;
-            }
-
-            if(isOdd) { // 짝수일땐 맨 밑에 줄이 무의미
-                for(j=size; j!=0; j--)
-                    printf("%c", '*');
+                if(line<comp) i+=2;
+                else i-=2;
+                }
+            if(isOdd) { // 짝수일떈 맨 밑줄이 무의미 
+                for(j=size; j>0; j--)
+                    printf("*");
                 printf("\n");
             }
             break;
-
         default:
-            printf("입력값 오류");
+            printf("입력값 오류\n");
             break;
     }
     return 0;
