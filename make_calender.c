d761c1d5b9e14efa238bed11d06358dab9b731ae
chocotime <duckhoim@daum.net>
2017-04-02 05:58:55 +0900

    코드 최적화
    1. 불필요하게 함수 호출 -> 배열로 변경
    2. 변하지 않는 값들을 굳이 포인터배열에 할당 -> 그냥 출력
    3. 10이하일때 뛰어쓰기 한번 더해서 줄맞춤 -> %2d로 더 간단
    4. do ~ while 이용하여 원하는 값을 입력하도록 요구
    5. 의미있고 짧은 변수명 선택 
        start_month -> sum_rem
    6. 불필요한 변수 제거 -> end_month 
    7. 이 커밋을 보고있군요. 반가워요.

--- make_calender.c
+++ make_calender.c
@@ -1,61 +1,32 @@
-#include <stdio.h>
-#define YEAR_START 0 // 1월 1일이 일요일
-int daysInMonth(int month) {
-    switch(month) {
-        case 2:
-            return 28;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-            return 30;
-        default:
-            return 31;
-    }
-}
-
-void showCalender(int month) {
-    int i;
-    char* week[7]={"일", "월", "화", "수", "목", "금", "토"};
-    int day=1;
-    int start_month=YEAR_START;
-    int end_month=daysInMonth(month);
-    // 월
-    printf("   %d월\n", month);
-    // 요일
-    for(i=0; i<7; i++)
-        printf("%s ", week[i]);
-    printf("\n");
-    // 비는 공백 계산
-    for(i=1; i<month; i++) {
-        start_month+=(daysInMonth(i)-28);
-        start_month%=7;
-    }
-    // 공백 먼저 출력
-    for(i=0; i<start_month; i++)
-        printf("   ");
-    // 요일 출력 ( 개행 까지 )
-    while(day<=end_month) {
-        if(day<10)
-            printf(" %d ", day);
-        else
-            printf("%d ", day);
-        if((start_month+day)%7==0)
-            printf("\n");
-        day++;
-    }
-    /*
-       7의 배수로 끝나면 개행 필요없음
-       마지막에 day++가 실행되고 끝남
-     */
-    if((start_month+day-1)%7!=0)
-        printf("\n");
-}
-int main(void) {
-    int month;
-    scanf("%d", &month);
-    if(month>12 || month<1)
-        printf("[입력값 오류] 1~12 사이의 값으로 입력해주세요.");
-    showCalender(month);
-    return 0;
-}
+  1 #include <stdio.h>
+  2 #define YEAR_START 0 // 1월 1일이 일요일
+  3 void showCalender(int mon) {
+  4     int i;
+  5     int days[12]={31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  6     int day; // 요일
+  7     int sum_rem=YEAR_START;
+  8     // 비는 공백 계산
+  9     for(i=0; i<mon-1; i++)  sum_rem=(sum_rem + (days[i]-28)) % 7;
+ 10 
+ 11     printf("%4d월\n", mon);
+ 12     printf("%s\n", "일 월 화 수 목 금 토" );
+ 13 
+ 14     for(i=0; i<sum_rem; i++)    printf("   ");
+ 15 
+ 16     for(day=1; day<=days[mon-1]; day++) {
+ 17         printf("%2d ", day);
+ 18         if((sum_rem+day)%7==0)  printf("\n");
+ 19     }
+ 20     if((sum_rem+day-1)%7!=0)    printf("\n"); // 마지막 day++를 없애려고 1을 뺀다.
+ 21 }
+ 22 int main(void) {
+ 23     int mon;
+ 24     printf(" 몇 월 달력을 보시겠어요?\n");
+ 25     do {
+ 26         printf(" 1~12 사이의 값으로 입력해주세요.\n");
+ 27         printf(" : ");
+ 28         scanf("%d", &mon);
+ 29     } while(mon<1 || mon>12);
+ 30     showCalender(mon);
+ 31     return 0;
+ 32 }
