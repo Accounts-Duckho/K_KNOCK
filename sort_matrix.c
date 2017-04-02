2a8fa81a8d669c2e8c7488610c6e2b39b12fd3db
chocotime <duckhoim@daum.net>
2017-04-02 12:01:12 +0900

    역순추가 + 역순이용 ㄹ 정렬 재 구성
    역순을 쓰니까 ㄹ 개짧아짐

--- sort_matrix.c
+++ sort_matrix.c
@@ -1,12 +1,13 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 /* 사용한 정렬 : 선택 정렬
    기준을 잡고 기준과 이미 정해진
    수들을 제외한 수들중 최소 or 최대 값을 
    구해 기준과 바꾼다.
    그 작업을 반복하여 정렬시킨다. */
-void inc_sort(int **num, int total_size);
-void dec_sort(int **num, int total_size);
+void inc_sort(int **num, int table_size);
+void dec_sort(int **num, int table_size);
+void reverse_sort(int **num, int table_size);
 void ryle_sort(int **num, int n);
 
 int main(void) {
@@ -37,7 +38,8 @@ int main(void) {
     printf(" [ 정렬 ]\n");
     printf(" 1. 오름차순\n");
     printf(" 2. 내림차순\n");
-    printf(" 3. ㄹ형정렬\n");
+    printf(" 3. 역순\n");
+    printf(" 4. ㄹ형정렬\n");
     printf(" [ 선택 ]\n");
     printf(" : ");
     scanf("%d", &sort_choice);
@@ -50,6 +52,9 @@ int main(void) {
             dec_sort(num, n*n);     break;
 
         case 3:
+            reverse_sort(num, n);       break;
+
+        case 4:
             ryle_sort(num, n);      break;
 
         default:
@@ -66,17 +71,17 @@ int main(void) {
     return 0;
 }
 
-void inc_sort(int **num, int total_size) {
+void inc_sort(int **num, int table_size) {
     int i, j, temp;
     int pt, min;
     int min_pt;
     /* [0][N-1] 다음 [1][0] 이런 식으로
        연속적으로 할당 되있다.
      */
-    for(pt=0; pt<total_size-1; pt++) {
+    for(pt=0; pt<table_size-1; pt++) {
         min=*(*num+pt);
         min_pt=pt;
-        for(i=pt+1; i<total_size; i++) {
+        for(i=pt+1; i<table_size; i++) {
             if(*(*num+i)<min) {
                 min=*(*num+i);
                 min_pt=i;
@@ -90,15 +95,15 @@ void inc_sort(int **num, int total_size) {
         }
     }
 }
-void dec_sort(int **num, int total_size) {
+void dec_sort(int **num, int table_size) {
     int i, j, temp;
     int pt, max;
     int max_pt;
 
-    for(pt=0; pt<total_size-1; pt++) {
+    for(pt=0; pt<table_size-1; pt++) {
         max=*(*num+pt);
         max_pt=pt;
-        for(i=pt+1; i<total_size; i++) {
+        for(i=pt+1; i<table_size; i++) {
             if(*(*num+i)>max) {
                 max=*(*num+i);
                 max_pt=i;
@@ -112,56 +117,35 @@ void dec_sort(int **num, int total_size) {
         }
     }
 }
+void reverse_sort(int **num, int n) {
+    int i, temp;
+    int table_size=n*n;
+    int break_pt;
+    if(n&1)     break_pt=(table_size-1)/2;
+    else        break_pt=table_size/2;
+    for(i=0; i<break_pt; i++) {
+        temp=*(*num+i);
+        *(*num+i)=*(*num+(table_size-1)-i);
+        *(*num+(table_size-1)-i)=temp;
+    }
+}
 void ryle_sort(int **num, int n) {
-    int sort_mode=1; // 1: inc, 0:dec
-    int i, j, temp;
-    int min, max;
-    int total_size=n*n;
-    int pt, min_pt, max_pt;
-    int end_pt=n;
-
+    int i, temp;
+    int table_size=n*n;
+    int pt, break_pt;
+    int comp;
     // 일단 배열 전체적으로 정렬
-    inc_sort(num, total_size);
+    inc_sort(num, table_size);
 
-    // 열마다 정렬방식을 바꿈
-    for(pt=0; pt<total_size; pt++) {
-        if((pt+1)%n==0) {
-            //  ^ : 두 비트가 다르면 1 같으면 0
-            sort_mode^=1;
-            end_pt+=n;
-            // 각 열의 마지막 행은 이미 정렬된 상태
-            continue;
-        }
-        // 1 - true, 0 - false
-        if(sort_mode) {
-            min=*(*num+pt);
-            min_pt=pt;
-            for(i=pt+1; i<end_pt; i++) {
-                if(*(*num+i)<min) {
-                    min=*(*num+i);
-                    min_pt=i;
-                }
-            }
-            if(*(*num+min_pt) < *(*num+pt)) {
-                temp=*(*num+pt);
-                *(*num+pt)=min;
-                *(*num+min_pt)=temp;
-            }
-        }
-        else {
-            max=*(*num+pt);
-            max_pt=pt;
-            for(i=pt+1; i<end_pt; i++) {
-                if(*(*num+i)>max) {
-                    max=*(*num+i);
-                    max_pt=i;
-                }
-            }
-            if(*(*num+max_pt) > *(*num+pt)) {
-                temp=*(*num+pt);
-                *(*num+pt)=max;
-                *(*num+max_pt)=temp;
-            }
+    // 짝수 열마다 정렬방식을 바꿈
+    for(pt=n; pt<table_size; pt+=2*n) {
+        break_pt=pt;
+        if(n&1)     break_pt+=(n-1)/2;
+        else        break_pt+=n/2;
+        for(i=pt; i<break_pt; i++) {
+            temp=*(*num+i);
+            *(*num+i)=*(*num+2*break_pt-i-((n+1)&1));
+            *(*num+2*break_pt-i-((n+1)&1))=temp;
         }
     }
 }
