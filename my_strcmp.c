e6e9aef53fde250fe919fcf320ca1ac14d2eec19
chocotime <duckhoim@daum.net>
2017-04-08 19:41:02 +0900

    Add files via upload

---
+++ my_strcmp.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+int my_cmp(char *s1, char *s2) {
+	do {
+		s1++;
+		s2++;
+		if(*s1 == '\0')
+			return *s1-*s2;
+	} while(*s1 == *s2);
+	return *s1-*s2;
+}		
+int main(void) {
+	printf("%d", my_cmp("손나은", "손새은"));
+	return 0;
+}
