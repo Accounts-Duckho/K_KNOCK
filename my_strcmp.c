d7e4af61b8b0f118a9d8c1a6ebaf785fc7d69001
chocotime <duckhoim@daum.net>
2017-04-11 18:38:30 +0900

    아아아아아아 누구야git add .! 이 신박한 비교를 생각한분은git add .

--- my_strcmp.c
+++ my_strcmp.c
@@ -1,14 +1,20 @@
 ﻿#include <stdio.h>
+#include <string.h>
 int my_cmp(char *s1, char *s2) {
+	printf("%d", strchr(s1, '\0')-s1);
 	do {
 		s1++;
 		s2++;
-		if(*s1 == '\0')
+		// 아래와 같은 상황일 경우에 대한 대처
+		// 더 좋은 방안을 찾고 있습니다..
+		if(*s1 == '\0' && *(s1+1)=='\0' )
 			return *s1-*s2;
 	} while(*s1 == *s2);
 	return *s1-*s2;
 }		
 int main(void) {
-	printf("%d", my_cmp("손나은", "손새은"));
+	printf("%d", my_cmp("abc\0de", "손나은"));
+	printf("%d", strcmp("\0", ""));
 	return 0;
 }
+	
