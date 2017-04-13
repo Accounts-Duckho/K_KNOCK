bc4c4ca32fbff88612835ad2b77260aa125b7a3b
chocotime <duckhoim@daum.net>
2017-04-14 02:57:51 +0900

    0413 스터디 가볍게 완료~

---
+++ draw_art.c
@@ -0,0 +1,101 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+typedef struct ArrStruct {
+	int **timewheel;
+	int **nabi;
+	int size_arr;
+	} ArrStruct;
+
+void create(ArrStruct *arr, int num);
+void print(ArrStruct *arr);
+
+int main(void) {
+	ArrStruct *art;
+	int num;
+	printf("크기 입력 : ");
+	scanf("%d", &num);
+	create(art, num);
+	print(art);
+
+	// free
+	if(!(num&1)) num--;
+	while(num--) {
+		free(art->timewheel[num]);
+		free(art->nabi[num]);
+	}
+	free(art->timewheel);
+	free(art->nabi);
+    return 0;
+}
+
+void create(ArrStruct *arr, int num) {
+    int i, j, line, mid;
+	int len = (num&1) ? num : num-1;
+
+	arr->timewheel=(int **)malloc(sizeof(int *) * len);
+	for(i=0; i<len; i++) 
+		arr->timewheel[i]=(int *)malloc(sizeof(int) * num);
+
+	arr->nabi=(int **)malloc(sizeof(int *) * len);
+	for(i=0; i<len; i++) 
+		arr->nabi[i]=(int *)malloc(sizeof(int) * num);
+
+	arr->size_arr=num;
+
+	// 줄 번호가 0부터 시작
+    mid = (len-1)/2;
+
+// 모래시계
+	i = num;
+	for(line=0; line<len; line++) {
+    	for(j=0; j<num; j++) {
+        	if(j<(num-i)/2) 
+				arr->timewheel[line][j]=0;
+            else if(j<(num+i)/2) 
+				arr->timewheel[line][j]=1;
+            else 
+				arr->timewheel[line][j]=0;
+		}
+        if(line<mid) i-=2;
+        else i+=2;
+	}
+
+// 나비
+	i =  num-2;
+    for(line=0;line<len;line++) {
+    	for(j=0; j<num; j++) {
+        	if(j<(num-i)/2) 
+				arr->nabi[line][j]=1;
+            else if(j<(num+i)/2) 
+				arr->nabi[line][j]=0;
+            else 
+				arr->nabi[line][j]=1;
+		}
+        if(line<mid) i-=2;
+        else i+=2;
+	}
+}
+
+void print(ArrStruct *arr) {
+	int size=arr->size_arr;
+	int i, j;
+	int len=(size&1) ? size : size-1;
+
+// 모래시계
+	printf("== 모래시계  출력  ==\n");
+	for(i=0; i<len; i++) {
+		for(j=0; j<size; j++)
+			printf("[%d]", arr->timewheel[i][j]);
+		printf("\n");
+	}
+	printf("== == == === == == ==\n");
+
+// 나비
+	printf("== == 나비 출력 == ==\n");
+	for(i=0; i<len; i++) {
+		for(j=0; j<size; j++)
+			printf("[%d]", arr->nabi[i][j]);
+		printf("\n");
+	}
+	printf("== == == === == == ==\n");
+}
---
+++ get_pocket.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+typedef struct Pocket {
+	char name[20];
+	char type[20];
+	int age;
+	int hp;
+} Pocket;
+char* pocket_name[6] = {"치코리타", "이상해씨", "파이리", "리자몽", "꼬부기", "야도란"};
+char* pocket_type[6] = {"풀", "풀", "불꽃", "불꽃, 비행", "물", "물, 에스퍼"};
+Pocket *create() {
+	srand((unsigned)time(NULL));
+	Pocket *mon=(Pocket *)malloc(sizeof(Pocket));
+	int i=rand()%6;
+	strcpy(mon->name, pocket_name[i]);
+	strcpy(mon->type, pocket_type[i]);
+	mon->age=rand()%101;
+	mon->hp=mon->age*500;
+	return mon;
+}	
+int main(void) {
+	Pocket *pocket=create();
+	printf("== 오늘의 포켓몬은 ? ==\n");
+	printf("%s\n", pocket->name);
+	printf("%s\n", pocket->type);
+	printf("%d\n", pocket->age);
+	printf("%d\n", pocket->hp);
+	printf("== == == == == == == ==\n");
+	return 0;
+}
